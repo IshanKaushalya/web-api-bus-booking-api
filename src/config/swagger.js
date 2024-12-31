ecb2298710bbfa390d83a22df2bcc02c454ef181
IshanKaushalya <ikishankaushalya@gmail.com>
2024-12-31 11:51:03 +0530

    add production email

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -14,6 +14,9 @@ const options = {
       {
         url: "http://localhost:5000",
       },
+      {
+        url: "https://web-api-bus-booking-api-production-2c85.up.railway.app",
+      },
     ],
   },
   apis: ["./src/routes/*.js"], // Path to the API docs
