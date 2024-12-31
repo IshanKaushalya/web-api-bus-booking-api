4fad4d9db760e20c77bcfb0377be145e42ecbce5
IshanKaushalya <ikishankaushalya@gmail.com>
2024-12-31 11:16:19 +0530

    update bycrypt into bycrypt js

--- src/routes/operator.js
+++ src/routes/operator.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
 const Operator = require("../models/Operator");
 const ScheduledTrip = require("../models/ScheduledTrip");
